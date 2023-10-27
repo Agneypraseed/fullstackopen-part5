dad2fb6187ab68e1519a1f6a2be57ada8592c1d5
Agney Praseed <agneysince2000@gmail.com>
2023-10-27 22:23:38 +0530

    5.18: bloglist end to end testing, step2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      username: "tester",
+      name: "Test User",
+      password: "test",
+    };
+    cy.request("POST", "http://localhost:3003/api/users", user);
     cy.visit("http://localhost:5173");
   });
 
@@ -9,5 +15,23 @@ describe("Blog app", function () {
     cy.contains("username");
     cy.contains("password");
     cy.contains("login");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("tester");
+      cy.get("#password").type("test");
+      cy.get("#login-button").click();
+
+      cy.contains("tester logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("tester");
+      cy.get("#password").type("fail");
+      cy.get("#login-button").click();
+
+      cy.contains("Wrong username or password");
+    });
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,7 @@ const App = () => {
         username
         <input
           type="text"
+          id="username"
           value={username}
           name="Username"
           onChange={({ target }) => setUsername(target.value)}
@@ -71,13 +72,16 @@ const App = () => {
       <div>
         password
         <input
+          id="password"
           type="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">
+        login
+      </button>
     </form>
   );
 
